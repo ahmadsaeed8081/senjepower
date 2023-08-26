822f9851389e7a40ee704a5ca3d61b6a8e16ef59
ahmad <ahmadsaeed8081@gmail.com>
2023-08-26 17:16:17 +0500

    changes done

---
+++ src/App.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./css/App.scss";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import {SPC} from "./components/SPC.ts"
+import Main from "./Pages/Home";
+
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { polygonMumbai} from 'wagmi/chains'
+
+
+
+import Web3 from "web3";
+
+const web3 = new Web3(Web3.givenProvider);
+
+function App() {
+  const [_address, set_address] = useState(null);
+  const [web3, set_web3] = useState(null);
+  const [provider, set_provider] = useState(null);
+  
+  const [isWalletConnected, set_isWalletConnected] = useState(false);
+
+
+  const chains = [SPC]
+const projectId = '8b3cd7dcbfb565705532d880d73759f2'
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+
+
+
+  function set_user(_add,_provider,_web3){
+    console.log("ihjono")
+    set_isWalletConnected(true);
+    set_provider(_provider);
+    set_web3(_web3);
+
+  }
+
+
+  return (
+    <div className="App">
+
+    <WagmiConfig config={wagmiConfig}>
+
+      {/* <Header set_user={set_user}/> */}
+      <Routes>
+        <Route path="/" element={<Main />} exact />
+      </Routes>
+      {/* <Footer /> */}
+    </WagmiConfig>
+
+<Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+</div>
+
+
+  );
+}
+
+export default App;
